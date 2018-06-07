62c5d64ea4b92dfb11268281d7099cb98ebb37b6
Jeremy Levett <jlevett.software@gmail.com>
2018-06-07 12:58:46 +1000

    feat: made basic book component

--- src/App.js
+++ src/App.js
@@ -1,37 +1,12 @@
 import React, { Component } from 'react'
-import BookShelf from'./components/Bookshelf.js'
-
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 
+import BookShelf from'./components/Bookshelf.js'
 
-//Move into state later on.
-const books = [
-            {
-            "width" :128,
-            "height" : 193,
-            "backgroundImage" : "http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api",
-            "status" : "Currently Reading", //Want to Read, Read, None, Currently Reading
-            "book-title": "1776 Big Ones",
-            "book-author": "David McCullough"
-            },
-            {
-            "width" :128,
-            "height" : 193,
-            "backgroundImage" : "http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api",
-            "status" : "Currently Reading", //Want to Read, Read, None, Currently Reading
-            "book-title": "1776 Big Ones",
-            "book-author": "David McCullough"
-            },
-            {
-            "width" :128,
-            "height" : 193,
-            "backgroundImage" : "http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api",
-            "status" : "Currently Reading", //Want to Read, Read, None, Currently Reading
-            "book-title": "1776 Big Ones",
-            "book-author": "David McCullough"
-            },
-]
+
+
+
 
 
 class BooksApp extends React.Component {
@@ -42,8 +17,37 @@ class BooksApp extends React.Component {
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
+  books : [
+              {
+              "backgroundImage" : "http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api",
+              "shelf" : "Read", //Want to Read, Read, None, Currently Reading
+              "bookTitle": "FirstTitle",
+              "bookAuthor": "1David McCullough"
+              },
+              {
+              "backgroundImage" : "http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api",
+              "shelf" : "Want to Read", //Want to Read, Read, None, Currently Reading
+              "bookTitle": "SecondTitle",
+              "bookAuthor": "2David McCullough"
+              },
+              {
+              "backgroundImage" : "http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api",
+              "shelf" : "Want to Read", //Want to Read, Read, None, Currently Reading
+              "bookTitle": "ThirdTitle",
+              "bookAuthor": "3dddaaad"
+              },
+              {
+              "backgroundImage" : "http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api",
+              "shelf" : "Currently Reading", //Want to Read, Read, None, Currently Reading
+              "bookTitle": "ForthTitle",
+              "bookAuthor": "4David McCullough"
+              }
+            ],
+
     showSearchPage: false
   }
+
+
 
   render() {
     return (
@@ -54,15 +58,25 @@ class BooksApp extends React.Component {
           </div>
           <div className="list-books-content">
             <div>
-                  <BookShelf title="Currently reading"/>
-                  <BookShelf title="Want to Read"/>
-                  <BookShelf title="Read"/>
+                  <BookShelf
+                    title="Currently Reading"
+                    selectedBooks={this.state.books.filter(book=>book.shelf==="Currently Reading")}
+                  />
+                  <BookShelf
+                    title="Want to Read"
+                    selectedBooks={this.state.books.filter(book=>book.shelf==="Want to Read")}
+                  />
+                  <BookShelf
+                    title="Read"
+                    selectedBooks={this.state.books.filter(book=>book.shelf==="Read")}
+                  />
+
             </div>
           </div>
         </div>
     </div>
     )
-  }
+  };
 }
 
 export default BooksApp
--- src/components/Bookshelf.js
+++ src/components/Bookshelf.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import Book from'./Book.js';
 
 //Stateless Functional Component
 function BookShelf(props){
+
 		return (
         	<div className="bookshelf">
                 <h2 className="bookshelf-title">
@@ -9,10 +11,15 @@ function BookShelf(props){
                 </h2>
                 <div className="bookshelf-books">
                 	<ol className="books-grid">
-                      {/*Enter Book Component here via filter*/}
+                		{/*List each book component out using map function*/}
+                		{props.selectedBooks.map((book, index)=>
+                			<Book key= {index} book = {book}/>
+                		)}
                     </ol>
                 </div>
             </div>
 		)
 }
 export default BookShelf;
+
+
