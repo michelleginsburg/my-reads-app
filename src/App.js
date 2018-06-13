fb2cdcb64ae932b773eab3666f52b0b9906f1f28
Jeremy Levett <jlevett.software@gmail.com>
2018-06-13 14:57:52 +1000

    feat:router/link added

--- src/App.js
+++ src/App.js
@@ -1,69 +1,73 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
+import { Route } from 'react-router-dom';
+
 import * as BooksAPI from './BooksAPI';
 import './App.css';
 
 import BookShelf from'./components/BookShelf.js';
 import Search from './components/Search.js';
-// import PropTypes from 'prop-types';
-
-
-
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    books : [],
-    showSearchPage: true,
+    books : []
   }
 
   /*Get the books avaliable on the bookshelf using the BookAPI getAll method*/
-  componentWillMount(){  //invoked immediately before the component is inserted into the DOM
+  //invoked immediately before the component is inserted into the DOM
+  componentWillMount(){
     BooksAPI.getAll().then(books=>{
         this.setState({books});
         });
   }
 
-  //TO DO: Update the book object to include new books choosen
-
   /*Update the shelf if a shelf selection is changed, if 'none' selected remove that book.*/
- //Search results on the search page allow the user to select “currently reading”, “want to read”, or “read” to place the book in a certain shelf.
   updateShelfForBook = (selectedBook, newShelf)=>{
-    //Update book on front end.
-   //ADD BOOK HERE from bookshelf using .splice(n, 0, selectedBook);
-    this.setState((prevState)=>({//Internal note: the state will be updated next time react calls render.
-        books:  prevState.books
-                  .map(book=>{ //Updatebooks to current shelves
-                    if(book.id===selectedBook.id)
-                      book.shelf = newShelf;
-                    return book;})
-                  .filter(b=>b.shelf!=='none') //remove from list if book selection is 'none'.
-    }));
+      //Update this.state.books.
+      this.setState(prevState=>{
+
+          let includeBook = true;
+          //Check if the selectedBook is in the bookshelf
+          prevState.books.forEach(function(book){
+              if(selectedBook.id === book.id){
+                    includeBook = false;
+              }
+          });
+        //If the book is not in the bookshelf then add it
+        if(includeBook)
+            prevState.books.splice(1, 0, selectedBook);
+
+        return ({
+          books:  prevState.books
+                      //Update books to current shelves
+                    .map(book=>{
+                      if(book.id===selectedBook.id)
+                         book.shelf = newShelf;
+                      return book;
+                    })
+                    //remove from list if book selection is 'none'.
+                    .filter(b=>b.shelf!=='none')
+        })
+      });
     //Update serverside with change.
     BooksAPI.update(selectedBook, newShelf);
-    console.log('updated');
   }
-
 
  render(){
     return (
       <div className="app">
-        {/*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/}
-        {this.state.showSearchPage ?
 
-
-          <Search
+        {/*Route/Display if on 'search' page*/}
+        <Route path = "/search" render={()=>(
+            <Search
              updateShelfForBook = {this.updateShelfForBook}
-          />
-
-         :
+             onNavigate = {this.onNavigate}
+            />
+        )}/>
+        {/*Route/Display if on 'main' page*/}
+        <Route exact path = "/" render={()=>(
           <div className="list-books">
-
-                   {/*Start: Book Shelf*/}
+            {/*Book Shelf*/}
                       <div className="list-books-title">
                       <h1>MyReads</h1>
                     </div>
@@ -73,6 +77,7 @@ class BooksApp extends React.Component {
                               title="Currently Reading"
                               selectedBooks={this.state.books.filter(book=>book.shelf==="currentlyReading")}
                               updateShelfForBook = {this.updateShelfForBook}
+
                             />
                             <BookShelf
                               title="Want to Read"
@@ -83,22 +88,22 @@ class BooksApp extends React.Component {
                               title="Read"
                               selectedBooks={this.state.books.filter(book=>book.shelf==="read")}
                               updateShelfForBook = {this.updateShelfForBook}
+
                             />
                       </div>
                     </div>
-                    {/*End: Book Shelf*/}
-
+            {/*Link to search Page*/}
             <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+              <Link to="/search">Add a book</Link>
             </div>
           </div>
-        }
-        {/*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/}
+        )}/>
+
+
       </div>
     )
   }
 }
-
 
 export default BooksApp
 
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,3 +1,6 @@
+import { Link } from 'react-router-dom'
+
+
 import React, { Component } from 'react';
 import Book from './Book';
 import * as BooksAPI from '../BooksAPI';
@@ -20,38 +23,50 @@ class Search extends Component {
 		showBooks: true
 	}
 
-
-
-
+//TO DO:
+//1. Cannot filter results book.title.toLowerCase().includes(query[0].toLowerCase()))
+//2. Books should be selected as none by default unless they are in the current bookshelf
+		//this.props.books. vs this.state.searchResults.
+//3.  Invalid queries are handled and prior search results are not shown.
+//4.Search results on the search page allow the user to select
+//“currently reading”, “want to read”, or “read” to place the book in a certain shelf.
     searchQuery = (query) =>{
 		//Update the input field
 		this.setState({query})
 		//Store results from multiple queries
 		let resultsSummary = [];
+		//Clear search results
+	    this.setState({searchResults:[],showBooks: false});
 
 	    //Get rid of white spaces on query, seperate string into words
 	    if(query.trim().length>0){
 			let queries = query.trim().split(" ");
 			let that = this;
 	    	queries.forEach(function(queryWord){
-
 				BooksAPI.search(queryWord).then(results => {
 						if (!results.error){
 		             			results.map(book => {
 		               				if(!book.imageLinks)
 		                		 			book.imageLinks = 'undefined';
+
 		                 			return book;
-		               			});
+		               			})
 								Array.prototype.push.apply(resultsSummary,results);
 								that.setState({searchResults: resultsSummary});
 		               		}
 				});
 	    	});
 	    	this.setState({showBooks: true});
+
+	    	//
+
+
 		  } else {
 	        	this.setState({searchResults:[],showBooks: false});
-	        	console.log('as');
+
 	      }
+
+
   	}
 
 
@@ -59,7 +74,10 @@ class Search extends Component {
 	    return (
 		 	<div className="search-books">
 	 	        <div className="search-books-bar">
-	 	            {/*<a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>*/}
+	 	            <Link className="close-search" to ="/"></Link>
+
+
+
 	 	            <div className="search-books-input-wrapper">
 		                <form>
 	 	            		<input
